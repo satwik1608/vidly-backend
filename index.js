4c4b017e45d6f5e3e8164883832b553c911381e4
satwik1608 <satwikkashyap1608@gmail.com>
2022-02-06 16:54:59 +0530

    Added customer and genres api and mongodb

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const Joi = require("joi");
 const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(console.log("Connecting to MongoDB..."))
+  .catch((err) => console.error("Some random error..."));
+
 app.use(express.json());
 app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
